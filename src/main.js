0f964a05219d4d8d37c47cfdb979bf9a5574451f
TomasMercuri <tomasmercuri12@gmail.com>
2023-07-18 00:57:05 -0300

    Agrega funcionalidades
    Cuando el usuario toque el boton "Comenzar", se cambiaran las imagenes de los cuadros, y luego podra jugar el usuario.
    Cada vez que toque un cuadro se ocultara el numero del cuadro y se mostrara la imagen del cuadro.

--- src/main.js
+++ src/main.js
@@ -6,15 +6,42 @@ function mostrarTablero(){
     document.querySelector('.juego').classList.remove('visually-hidden');
 }
 
-function retornarValorFormulario(){
+function retornarOpcion(){
     return $form.opcion.value;
 }
+
+function cambiarImagenDelCuadro(opcion){
+    opcion === 'autos' ? opcion = autos : opcion === 'emojis' ? opcion = emojis : opcion = colores;
+    document.querySelectorAll('.parte-trasera img').forEach(function(img, i){
+        img.src = opcion[i];
+    });
+}
+
+function ocultarParteDelantera(i){
+    document.querySelector(`.cuadro-${i} .parte-delantera`).classList.add('visually-hidden');
+}
+
+function mostrarParteTrasera(i){
+    document.querySelector(`.cuadro-${i} .parte-trasera`).classList.remove('visually-hidden');
+}
+
+function comenzarJuego(){
+    document.querySelectorAll('.cuadros').forEach(function(cuadro, i){
+        cuadro.addEventListener('click', function(){
+            ocultarParteDelantera(i + 1);
+            mostrarParteTrasera(i + 1);
+        });
+    });
+}
+
+
+
 
 const $form = document.querySelector('.formulario');
 
 document.querySelector('#comenzar').addEventListener('click', function(){
     ocultarOpciones();
-    setTimeout(mostrarTablero, 500);
-    const opcion = retornarValorFormulario();
-    
+    mostrarTablero()
+    cambiarImagenDelCuadro(retornarOpcion());
+    comenzarJuego();
 });
