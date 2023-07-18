d96fd05cd471c13bd710471b54518edb1ea4380e
TomasMercuri <tomasmercuri12@gmail.com>
2023-07-18 00:55:06 -0300

    Crea archivo js con las imagenes de los emojis

---
+++ src/emojis.js
@@ -0,0 +1,14 @@
+const emojis = [
+    '../img/emojis/emoji1.png',
+    '../img/emojis/emoji1.png',
+    '../img/emojis/emoji2.png',
+    '../img/emojis/emoji2.png',
+    '../img/emojis/emoji3.png',
+    '../img/emojis/emoji3.png',
+    '../img/emojis/emoji4.png',
+    '../img/emojis/emoji4.png',
+    '../img/emojis/emoji5.png',
+    '../img/emojis/emoji5.png',
+    '../img/emojis/emoji6.png',
+    '../img/emojis/emoji6.png'
+];
