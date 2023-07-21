d79377c346922119643fa9ebd7b8fcc5570e5b52
TomasMercuri <tomasmercuri12@gmail.com>
2023-07-21 00:03:34 -0300

    Agrega la logica del juego
    Cada vez que el usuario toque un cuadro, se mostrara.
    Cuando haya tocado dos cuadros, se bloquea la interaccion del usuario para que no siga tocando mas cuadros, comprueba si los cuadros son iguales y los elimina, en caso que no sean iguales los vuelve a ocultar. Cuando termina de comprobar y procesar, se desbloquea la interaccion del usuario.

--- src/main.js
+++ src/main.js
@@ -17,40 +17,74 @@ function cambiarImagenDelCuadro(opcion){
     });
 }
 
-function ocultarParteDelantera(i){
-    document.querySelector(`.cuadro-${i} .parte-delantera`).classList.add('visually-hidden');
+
+function mostrarImagenDelCuadro(cuadro){
+    cuadro.classList.remove('visually-hidden');
 }
 
-function mostrarParteTrasera(i){
-    document.querySelector(`.cuadro-${i} .parte-trasera`).classList.remove('visually-hidden');
+function ocultarImagenDelCuadro(primerCuadro, segundoCuadro){
+    setTimeout(function(){
+        primerCuadro.classList.add('visually-hidden');
+        segundoCuadro.classList.add('visually-hidden');
+    }, 1000);
 }
 
-function comprobarCuadrosIguales(cuadroActual, cuadroAnterior){
-    return cuadroActual === cuadroAnterior;
+function cuadrosSonIguales(primerCuadro, segundoCuadro){
+    return primerCuadro.innerHTML === segundoCuadro.innerHTML;
+}
+
+function eliminarCuadros(primerCuadro, segundoCuadro){
+    setTimeout(function(){
+        primerCuadro.classList.add('cuadro-oculto');
+        segundoCuadro.classList.add('cuadro-oculto');
+    }, 1000);
+}
+
+function bloquearInteraccionUsuario(){
+    document.querySelectorAll('.cuadros').forEach(function(cuadro){
+        cuadro.onclick = function(){};
+    });
+}
+
+function interaccionDelUsuario(cuadro){
+    if(cuadro !== cuadroAnterior && !cuadrosEliminados.includes(cuadro)){
+        contadorClicks++;
+        mostrarImagenDelCuadro(cuadro);
+        if(contadorClicks % 2 === 0){
+            bloquearInteraccionUsuario();
+            if(cuadrosSonIguales(cuadro, cuadroAnterior)){
+                eliminarCuadros(cuadro, cuadroAnterior);
+                cuadrosEliminados.push(cuadro, cuadroAnterior);
+                setTimeout(jugar, 1000);
+            }else{
+                ocultarImagenDelCuadro(cuadro, cuadroAnterior);
+                cuadro = '';
+                setTimeout(jugar, 1000);
+            }
+        }
+        cuadroAnterior = cuadro;
+    }
 }
 
 
-function comenzarJuego(){
-    let cuadroAnterior = 0;
+function jugar(){
     document.querySelectorAll('.cuadros').forEach(function(cuadro, i){
-        cuadro.addEventListener('click', function(){
-            const cuadroActual = document.querySelector(`.cuadro-${i + 1} .parte-trasera img`).src;
-            ocultarParteDelantera(i + 1);
-            mostrarParteTrasera(i + 1);
-            comprobarCuadrosIguales(cuadroActual, cuadroAnterior)
-            cuadroAnterior = cuadroActual;
-        });
+        cuadro.onclick = function(){
+            cuadro = document.querySelector(`.cuadro-${i + 1} .parte-trasera`);
+            interaccionDelUsuario(cuadro);
+        };
     });
 }
 
 
-
-
 const $form = document.querySelector('.formulario');
+let contadorClicks = 0;
+let cuadroAnterior = '';
+let cuadrosEliminados = [];
 
 document.querySelector('#comenzar').addEventListener('click', function(){
     ocultarOpciones();
     mostrarTablero()
     cambiarImagenDelCuadro(retornarOpcion());
-    comenzarJuego();
+    jugar();
 });
