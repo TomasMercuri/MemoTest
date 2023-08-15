f1e56854c7b154ecb74b1b89624880cde2556595
TomasMercuri <tomasmercuri12@gmail.com>
2023-08-15 19:27:16 -0300

    Modifica las rutas de las imagenes del juego
    Al mover los archivos index.html y styles.css, tambien hay que cambiar las rutas de las imagenes para que se puedan mostrar.

--- src/autos.js
+++ src/autos.js
@@ -1,14 +1,14 @@
 const autos = [
-    '../img/autos/auto1.jpg',
-    '../img/autos/auto1.jpg',
-    '../img/autos/auto2.jpg',
-    '../img/autos/auto2.jpg',
-    '../img/autos/auto3.jpg',
-    '../img/autos/auto3.jpg',
-    '../img/autos/auto4.jpg',
-    '../img/autos/auto4.jpg',
-    '../img/autos/auto5.jpg',
-    '../img/autos/auto5.jpg',
-    '../img/autos/auto6.jpg',
-    '../img/autos/auto6.jpg'
+    './img/autos/auto1.jpg',
+    './img/autos/auto1.jpg',
+    './img/autos/auto2.jpg',
+    './img/autos/auto2.jpg',
+    './img/autos/auto3.jpg',
+    './img/autos/auto3.jpg',
+    './img/autos/auto4.jpg',
+    './img/autos/auto4.jpg',
+    './img/autos/auto5.jpg',
+    './img/autos/auto5.jpg',
+    './img/autos/auto6.jpg',
+    './img/autos/auto6.jpg'
 ];
